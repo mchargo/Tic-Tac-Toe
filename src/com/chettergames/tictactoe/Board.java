af1e1bc2798eb2066d64a793c33f74e5f2958874
Mike Chargo <mike.chargo@sbcglobal.net>
2015-01-06 02:17:16 -0600

    implemented print board method

--- src/com/chettergames/tictactoe/Board.java
+++ src/com/chettergames/tictactoe/Board.java
@@ -4,13 +4,31 @@ public class Board
 {
 	public Board()
 	{
-		pieces = new char[3][3];
+		pieces = new Piece[3][3];
 	}
 	
 	public void printToConsole()
 	{
-		
+		for(int row = 0; row<3; row++)
+		{
+			System.out.print("|");
+			for(int col = 0; col<3; col++)
+			{
+				if(pieces[row][col]==null)
+				{
+					System.out.print(" ");
+				}else if(pieces[row][col].isX())
+				{
+					System.out.print("X");
+				}else if(!(pieces[row][col].isX()))
+				{
+					System.out.print("O");
+				}
+				System.out.print("|");
+			}
+			System.out.println();
+		}
 	}
 	
-	private char[][] pieces;
+	private Piece[][] pieces;
 }
